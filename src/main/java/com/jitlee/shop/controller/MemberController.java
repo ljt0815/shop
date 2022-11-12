ada8b6d076dbd5991a23827181e9c1d66075e6ee
ljt0815 <ljt0815@naver.com>
2022-11-13 03:34:55 +0900

    add loginForm, edit MemberController

--- src/main/java/com/jitlee/shop/controller/MemberController.java
+++ src/main/java/com/jitlee/shop/controller/MemberController.java
@@ -2,9 +2,14 @@ package com.jitlee.shop.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
 }
